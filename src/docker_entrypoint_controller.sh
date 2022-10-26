e8912e4be7f4750542498b04b45720263397f6ef
Julian Hess <jhess@broadinstitute.org>
2022-10-26 20:19:56 +0000

    Use 22.04 in Slurm docker

--- src/docker_entrypoint_controller.sh
+++ src/docker_entrypoint_controller.sh
@@ -4,7 +4,7 @@
 
 sudo -E /usr/local/share/slurm_gcp_docker/src/docker_copy_gcloud_credentials.sh
 
-sudo mysqld &
+sudo mysqld --user root &
 /usr/local/share/slurm_gcp_docker/src/provision_server.py
 export SLURM_CONF=/mnt/nfs/clust_conf/slurm/slurm.conf
 /bin/bash
