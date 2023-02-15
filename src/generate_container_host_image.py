bb486def1fb362db8e6c50577f437376e5334e95
Julian Hess <jhess@broadinstitute.org>
2023-02-15 20:56:48 +0000

    Initial commit of building master Docker/VM images

--- src/generate_container_host_image.py
+++ src/generate_container_host_image.py
@@ -8,15 +8,24 @@ import sys
 import shlex
 import tempfile
 
+from setup_local import check_gcloud_auth, check_docker, error
+
 def parse_args(zone, project):
-	parser = argparse.ArgumentParser()
+	parser = argparse.ArgumentParser(description =
+"""
+This builds the master Slurm Docker image and worker VM host image.
+This is only used by developers updating core functionality of this software package.
+
+Note that the Docker daemon must have experimental features enabled;
+add { "experimental": true } to /etc/docker/daemon.json
+""", formatter_class = argparse.RawTextHelpFormatter)
 	parser.add_argument('--imagename', '-i', help = "Name of image to create", required = True)
 	parser.add_argument('--zone', '-z', help = "Compute zone to create dummy instance in", default = zone)
 	parser.add_argument('--project', '-p', help = "Compute project to create image in", default = project)
 	parser.add_argument('--dummyhost', '-d', help = "Name of dummy VM image gets built on", default = "dummyhost")
 	parser.add_argument('--build_script', '-s', help = "Path to build script whose output is run on the dummy VM", default = "./container_host_image_startup_script.sh")
-	parser.add_argument('--dont_copy_gcloud_credentials', '-g', help = "Skip copying of gcloud credentials", action = "store_false", dest = "copy_gcloud_credentials")
 	parser.add_argument('--image_family', '-f', help = "Family to add image to", default = "slurm-gcp-docker")
+	parser.add_argument('--push_images', help = "Whether to push images to centeralized container regisitry/GCP project", action = "store_true")
 
 	args = parser.parse_args()
 
@@ -30,33 +39,27 @@ def parse_args(zone, project):
 
 	return args
 
-def get_slurm_gcp_docker_root():
-	# I placed a marker file ".slurm_gcp_docker_root"
-	src_dir = os.path.dirname(__file__)
-	src_parent = os.path.abspath(os.path.join(src_dir, os.path.pardir))
-	assert os.path.exists(os.path.join(src_parent, ".slurm_gcp_docker_root"))
-	return src_parent
-
 if __name__ == "__main__":
 	#
-	# ensure gcloud is installed
+	# check prerequisites
+	check_gcloud_auth()
+	check_docker()
 	try:
-		subprocess.check_call("[ -f ~/.config/gcloud/config_sentinel ]", shell = True)
+		subprocess.check_call("[ -f /etc/docker/daemon.json ] && grep -Eq 'experimental.*[Tt]rue' /etc/docker/daemon.json", shell = True)
 	except subprocess.CalledProcessError:
-		print("gcloud is not configured. Please run `gcloud auth login --update-adc` and try again.", file = sys.stderr)
-		sys.exit(1)
+		error('Experimental mode must be enabled in Docker (add { "experimental": true } to /etc/docker/daemon.json and restart Docker service)')
 
 	#
 	# get zone of current instance
 	default_zone = subprocess.check_output(
-		"curl -H 'Metadata-Flavor: Google' http://metadata.google.internal/computeMetadata/v1/instance/zone", shell=True
+		"curl -H 'Metadata-Flavor: Google' http://metadata.google.internal/computeMetadata/v1/instance/zone", shell=True, stderr = subprocess.DEVNULL
 	).decode().rstrip()
 	default_zone = default_zone.split("/")[-1]
 
 	#
 	# get current project (if any)
 	default_proj = subprocess.check_output(
-		'curl "http://metadata.google.internal/computeMetadata/v1/project/project-id" -H "Metadata-Flavor: Google"', shell=True
+		'curl "http://metadata.google.internal/computeMetadata/v1/project/project-id" -H "Metadata-Flavor: Google"', shell=True, stderr = subprocess.DEVNULL
 	).decode().rstrip()
 
 	#
@@ -69,6 +72,34 @@ if __name__ == "__main__":
 	#
 	# get hostname
 	host = args.dummyhost + "-" + os.environ["USER"]
+
+	#
+	# 1. build Docker image
+	#
+
+	VERSION = open("VERSION").read().rstrip()
+
+	subprocess.check_call(f"""
+	  (cd .. &&
+	  sudo docker build --squash -t broadinstitute/slurm_gcp_docker:{VERSION} \
+		-t broadinstitute/slurm_gcp_docker:latest \
+		-f src/Dockerfile .)""", shell = True
+	)
+
+	if args.push_images:
+		subprocess.check_call(f"""
+		  docker tag broadinstitute/slurm_gcp_docker:{VERSION} \
+			gcr.io/broad-getzlab-workflows/slurm_gcp_docker:{VERSION} && \
+		  docker tag broadinstitute/slurm_gcp_docker:{VERSION} \
+			gcr.io/broad-getzlab-workflows/slurm_gcp_docker:latest && \
+		  docker push gcr.io/broad-getzlab-workflows/slurm_gcp_docker:{VERSION} && \
+		  docker push gcr.io/broad-getzlab-workflows/slurm_gcp_docker:latest""",
+		  shell = True
+		)
+
+	#
+	# 2. build VM worker image
+	# 
 
 	#
 	# create dummy instance to build image in
@@ -94,45 +125,13 @@ if __name__ == "__main__":
 		)
 
 		#
-		# copy gcloud config to instance
-		if args.copy_gcloud_credentials:
-			print("Copying gcloud credentials to dummy host ...")
-			subprocess.check_call("""
-			  gcloud compute scp ~/.config/gcloud/* {host}:.config/gcloud --zone {zone} --recurse && \
-			  gcloud compute ssh {host} --zone {zone} -- -o "StrictHostKeyChecking no" -o "UserKnownHostsFile /dev/null" -T \
-				"sudo cp -r ~/.config/gcloud /etc/gcloud"
-			  """.format(host = host, zone = zone),
-			  shell = True
-			)
-
-		#
-		# copy slurm_gcp_docker source to instance and build docker there
-		print("Copying slurm_gcp_docker source to dummy host ...")
-		subprocess.check_call(
-			"gcloud compute scp {src} {host}:/tmp/tmp_slurm_gcp_docker --zone {zone} --recurse".format(
-				src=shlex.quote(get_slurm_gcp_docker_root()), host = host, zone = zone
-			),
-			shell=True
-		)
-
-		#
-		# transfer docker image there
+		# transfer Slurm Docker image to instance
 		print("Transfering slurm docker image to dummy host ...")
-		DOCKER_SRC = open("DOCKER_SRC").read().rstrip()
-		VERSION = open("VERSION").read().rstrip()
 
 		tmp = tempfile.mktemp()
-		subprocess.check_call("sudo docker save {}:{} > {}".format(DOCKER_SRC, VERSION, tmp), shell=True)
-		subprocess.check_call("gcloud compute scp {src} {host}:/tmp/tmp_docker_file --zone {zone}".format(src=tmp, host=host, zone=zone), shell=True)
+		subprocess.check_call("sudo docker save broadinstitute/slurm_gcp_docker:{} > {}".format(VERSION, tmp), shell=True)
+		subprocess.check_call('gcloud compute scp {src} {host}:/tmp/tmp_docker_file --zone {zone} && gcloud compute ssh {host} --zone {zone} -- -o "UserKnownHostsFile" /dev/null sudo touch /data_transferred'.format(src=tmp, host=host, zone=zone), shell=True)
 		os.remove(tmp)
-
-		#
-		# mark data transferred
-		subprocess.check_call(
-			'gcloud compute ssh {host} --zone {zone} -- -o "StrictHostKeyChecking no" -o "UserKnownHostsFile /dev/null" -T \
-				"sudo cp -r /tmp/tmp_slurm_gcp_docker/ /usr/local/share/slurm_gcp_docker/ && sudo touch /data_transferred"'.format(host = host, zone = zone),
-			shell=True
-		)
 
 		#
 		# wait for startup script to be completed
@@ -175,6 +174,10 @@ if __name__ == "__main__":
 			  "gcloud compute images create {imagename} --source-snapshot={host}-snap --family {image_family}-$USER".format(imagename = imagename, host = host, image_family = args.image_family),
 			  shell = True
 			)
+
+			# TODO: move image to central project accessible to all
+			if args.push_images:
+				pass
 		finally:
 			print("Deleting snapshot ...")
 			subprocess.check_call("gcloud compute snapshots delete {}-snap --quiet".format(host), shell = True)
