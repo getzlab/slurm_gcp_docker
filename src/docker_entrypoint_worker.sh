f80f7f8db675d45144ba4659d0c13f46b50f77d3
Julian Hess <jhess@broadinstitute.org>
2023-02-15 23:28:19 +0000

    I don't think this needs to be sudo'd, since the worker container runs as root anyway?

--- src/docker_entrypoint_worker.sh
+++ src/docker_entrypoint_worker.sh
@@ -2,7 +2,7 @@
 
 . /usr/local/share/slurm_gcp_docker/src/docker_init_credentials.sh
 
-sudo -E /usr/local/share/slurm_gcp_docker/src/docker_copy_gcloud_credentials.sh
+/usr/local/share/slurm_gcp_docker/src/docker_copy_gcloud_credentials.sh
 
 . /usr/local/share/slurm_gcp_docker/src/slurm_start.sh
 /usr/local/share/slurm_gcp_docker/src/container_heartbeat.sh &
