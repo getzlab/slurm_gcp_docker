53bf088f4e8dc6a17d2da75188e2bb2c59e2dfcf
Julian Hess <jhess@broadinstitute.org>
2023-02-17 14:04:40 +0000

    Move NFS provisioning to startup script
    Since we no longer copy any cluster provisioning scripts to the bare worker VM,
    we need to explicitly embed this in the startup script.

--- src/provision_worker_container_host.sh
+++ src/provision_worker_container_host.sh
@@ -3,7 +3,32 @@
 set -e
 
 # mount NFS
-/usr/local/share/slurm_gcp_docker/src/nfs_provision_worker.sh ${1}
+SHOST=$1
+
+echo "Starting NFS ..."
+
+# check if mount is stale
+timeout 30 stat -t /mnt/nfs &> /dev/null
+EC=$?
+if [[ $EC == 124 ]]; then
+	# attempt to unmount
+	echo -n "NFS mount is stale; attempting unmount ..." > /dev/stderr
+	if ! sudo timeout 2 umount -f /mnt/nfs; then
+		echo -e "\nUnmount failed. Please close any open files (check with \`lsof -b | grep /mnt/nfs\`) and then \`sudo umount -f /mnt/nfs\`." > /dev/stderr
+		exit 1
+	fi
+	echo " success!"
+fi
+
+# otherwise, wait for mount to be ready (NFS server is starting up)
+echo -n "Waiting for NFS server to be ready ..."
+[ ! -d /mnt/nfs ] && sudo mkdir -p /mnt/nfs
+while ! mountpoint -q /mnt/nfs; do
+	sudo mount -o defaults,hard,intr ${SHOST}:/mnt/nfs /mnt/nfs &> /dev/null
+	echo -n "."
+	sleep 1
+done
+echo
 
 # start Slurm docker
-. /usr/local/share/slurm_gcp_docker/src/docker_run.sh
+. /mnt/nfs/clust_scripts/docker_run.sh
