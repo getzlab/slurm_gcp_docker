b2679d0f0337cd9dfbe9239831faf4f5fb3dbd7b
Julian Hess <jhess@broadinstitute.org>
2023-03-01 22:44:39 +0000

    Auto-set version

--- setup.py
+++ setup.py
@@ -1,6 +1,7 @@
 from setuptools import setup
 from src import test_controller_environment
 
+import os
 import subprocess
 
 # ensure prerequisites are satisfied.
@@ -9,9 +10,13 @@ test_controller_environment.check_all()
 # pull Docker
 subprocess.run("docker pull gcr.io/broad-getzlab-workflows/slurm_gcp_docker:latest", shell = True)
 
+# get version
+with open(f"{os.path.dirname(__file__)}/src/VERSION") as v:
+    version = v.read().rstrip()[1:]
+
 setup(
     name = 'slurm_gcp_docker',
-    version = "1.0.0",
+    version = version,
     description = 'An autoscaling Slurm cluster on Google Cloud Platform',
     url = 'https://github.com/getzlab/slurm_gcp_docker',
     author = 'Julian Hess/Jialin Ma/Oliver Priebe - Broad Institute - Cancer Genome Computational Analysis',
