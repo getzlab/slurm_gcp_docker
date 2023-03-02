a149594e8ebfa10ed428c3a6fc1f9a76478c1057
Julian Hess <jhess@broadinstitute.org>
2023-03-02 18:13:11 +0000

    Forgot trailing slash

--- src/docker_copy_gcloud_credentials.sh
+++ src/docker_copy_gcloud_credentials.sh
@@ -2,14 +2,14 @@
 
 # add gcloud credentials to slurm's home directory
 [ ! -d ~slurm/.config/gcloud ] && mkdir -p ~slurm/.config/gcloud
-cp -r /mnt/nfs/credentials/gcloud ~slurm/.config/gcloud && \
+cp -r /mnt/nfs/credentials/gcloud/ ~slurm/.config/gcloud && \
 chown -R slurm:slurm ~slurm/.config/gcloud && \
 ln -s ~slurm/.config/gcloud /slurm_gcloud_config
 
 # add gcloud credentials to user's home directory
 HOMEDIR=`eval echo ~$HOST_USER`
 [ ! -d $HOMEDIR/.config/gcloud ] && mkdir -p $HOMEDIR/.config/gcloud
-cp -r /mnt/nfs/credentials/gcloud $HOMEDIR/.config/gcloud && chown -R $HOST_USER:$HOST_USER $HOMEDIR/.config/
+cp -r /mnt/nfs/credentials/gcloud/ $HOMEDIR/.config/gcloud && chown -R $HOST_USER:$HOST_USER $HOMEDIR/.config/
 ln -s $HOMEDIR/.config/gcloud /user_gcloud_config
 
 # add Docker credentials to user's home directory
