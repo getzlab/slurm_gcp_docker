396ae04e787c3f352788c7b8e7130cecf82b9081
Julian Hess <jhess@broadinstitute.org>
2023-03-02 16:28:27 +0000

    Source worker startup script directly
    /sgcpd only exists inside the Docker container, and we don't copy over any
    of the cluster scripts anymore (nor expose them on the NFS). However, docker_run.sh
    is the only script that's needed to run outside of the Docker container on a worker,
    so we just embed it in the startup script.

--- src/worker_startup_script.sh
+++ src/worker_startup_script.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-# mount NFS
+## mount NFS
 CONTROLLER_NAME=$(curl "http://metadata.google.internal/computeMetadata/v1/instance/attributes/slurm-controller-hostname" -H "Metadata-Flavor: Google")
 
 echo "Starting NFS ..."
@@ -31,5 +31,22 @@ while ! mountpoint -q /mnt/nfs; do
 done
 echo
 
-# start Slurm docker
-. /sgcpd/src/docker_run.sh
+## start Slurm docker
+
+# resolves occasional quota exceeded error, per https://stackoverflow.com/questions/54405454/error-response-from-daemon-join-session-keyring-create-session-key-disk-quota
+# these values come from root_max{keys,bytes}
+echo 1000000 > /proc/sys/kernel/keys/maxkeys
+echo 25000000 > /proc/sys/kernel/keys/maxbytes
+
+# only set gpu flags if gpu is attached
+nvidia-smi && GPU_FLAGS="--gpus all"
+# allow docker to use full extent of shared memory
+SHM_SIZE=$(df -h -BM --output=size /dev/shm | sed 1d | awk '{print tolower($0)}')
+
+# start the container
+docker run -dti --rm --pid host --network host --privileged \
+  -v /mnt/nfs:/mnt/nfs -v /sys/fs/cgroup:/sys/fs/cgroup \
+  -v /var/run/docker.sock:/var/run/docker.sock -v /usr/bin/docker:/usr/bin/docker \
+  -v /dev:/dev ${GPU_FLAGS} --shm-size ${SHM_SIZE} \
+  --entrypoint /sgcpd/src/docker_entrypoint_worker.sh --name slurm \
+  broadinstitute/slurm_gcp_docker
