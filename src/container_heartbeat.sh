d1cfad19f7e93167b33cbb55089c168ebc4ff56f
Julian Hess <jhess@broadinstitute.org>
2023-03-14 14:34:51 +0000

    Log all container heartbeat output to file

--- src/container_heartbeat.sh
+++ src/container_heartbeat.sh
@@ -5,27 +5,28 @@
 
 export CLOUDSDK_CONFIG=/slurm_gcloud_config
 
-# get zone of instance
-ZONE=$(basename $(curl -H "Metadata-Flavor: Google" http://metadata.google.internal/computeMetadata/v1/instance/zone 2> /dev/null))
-
 # create instance logfile
 export LOGFILE=/mnt/nfs/clust_logs/${HOSTNAME}.heartbeat.log
 [ -f $LOGFILE ] && rm -f $LOGFILE
+exec > $LOGFILE 2>&1
+
+# get zone of instance
+ZONE=$(basename $(curl -H "Metadata-Flavor: Google" http://metadata.google.internal/computeMetadata/v1/instance/zone 2> /dev/null))
 
 # run separate daemon to detect hung disks
-/sgcpd/src/hung_disk_daemon.py >> $LOGFILE 2>&1 &
+/sgcpd/src/hung_disk_daemon.py &
 
 while true; do
 	# check if Podman is responsive
-	if ! timeout 300 podman info; then
-		echo "`date` podman flatlined" >> $LOGFILE
+	if ! timeout 300 podman info &> /dev/null; then
+		echo "`date` podman flatlined"
 		scontrol update nodename=$HOSTNAME state=FAIL reason="podman flatlined" && \
 		gcloud compute instances delete $HOSTNAME --zone $ZONE --quiet
 	fi
 
 	# check if disk is full (<5% space remaining on root partition)
 	if ! df / | awk 'NR == 2 { if($4/($4 + $3) < 0.05) { exit 1 } }'; then
-		echo "`date` local disk full" >> $LOGFILE
+		echo "`date` local disk full"
 		scontrol update nodename=$HOSTNAME state=FAIL reason="local disk full" && \
 		gcloud compute instances delete $HOSTNAME --zone $ZONE --quiet
 	fi
@@ -33,7 +34,7 @@ while true; do
 	# check if this node had problems attaching a disk (as reported by a task's
 	# localization script)
 	if [ -f /.fatal_disk_issue_sentinel ]; then
-		echo "`date` fatal disk issue" >> $LOGFILE
+		echo "`date` fatal disk issue"
 		scontrol update nodename=$HOSTNAME state=FAIL reason="disk attach problems" && \
 		gcloud compute instances delete $HOSTNAME --zone $ZONE --quiet
 	fi
@@ -42,7 +43,7 @@ while true; do
 	timeout 30 bash -c 'scontrol ping | grep -q "is DOWN"'
 	RC=$?
 	if [[ $RC == 124 || $RC == 0 ]]; then # 124 -> timeout; 0 -> grep succeeded
-		timeout 1 bash -c "echo \"`date` self-destructing due to nonresponsive controller!\" >> $LOGFILE"
+		timeout 1 bash -c "echo \"`date` self-destructing due to nonresponsive controller!\"
 		gcloud compute instances delete $HOSTNAME --zone $ZONE --quiet
 	fi
 
