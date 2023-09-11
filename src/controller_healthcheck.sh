96679c306d78ced10a3367a7867725d4efe7ceae
Julian Hess <jhess@broadinstitute.org>
2023-09-11 18:09:38 +0000

    Make controller disk resize poll interval more aggressive
    10 minutes between polls could allow the disk to fill up. Put disk resizing into
    its own script in order to poll more aggressively (Other 10 minute poll operations are
    expensive, i.e. slurm checks)

--- src/controller_healthcheck.sh
+++ src/controller_healthcheck.sh
@@ -10,22 +10,6 @@ while true; do
 	# resume all down hosts
 	scontrol update nodename=$(sinfo -h --format %N -t down -p all) state=resume
 
-	#
-	# increase disks' sizes
-	# for each persistent disk attached to instance,
-	for DISK_DEV in $(find /dev/disk/by-id -name "google*"); do
-		GOOGLE_DISK_NAME=$(sed 's|/dev/disk/by-id/google-||' <<< "$DISK_DEV")
-		# if this persistent disk is mounted to wolF NFS, check if it's low on space
-		if df $DISK_DEV | grep -q /mnt/nfs; then
-			DISK_SIZE_GB=$(df -B1G $DISK_DEV | awk 'NR == 2 { print int($3 + $4) }')
-			FREE_SPACE_GB=$(df -B1G $DISK_DEV | awk 'NR == 2 { print int($4) }')
-			if [[ $((100*FREE_SPACE_GB/DISK_SIZE_GB)) -lt 10 ]]; then
-				gcloud_exp_backoff compute disks resize $GOOGLE_DISK_NAME --quiet --zone $ZONE --size $((DISK_SIZE_GB+200))
-				sudo resize2fs $DISK_DEV
-			fi
-		fi
-	done
-
 	# release stuck jobs
 	squeue -t PD -o '%i'$'\t''%R' | awk -F'\t' '$2 == "(launch failed requeued held)" { print $1 }' | xargs scontrol release
 
