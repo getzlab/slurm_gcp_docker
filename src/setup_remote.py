1a079fcaa63dad670718a9889acc976f321eee84
SamWisedev <swiseman@broadinstitute.org>
2025-04-04 23:43:14 -0400

    hyperdisk-balanced typo

--- src/setup_remote.py
+++ src/setup_remote.py
@@ -61,7 +61,7 @@ def create_wolfcontroller(instance_name, project=None, zone=None, machine_type="
             --image ubuntu-minimal-2004-focal-v20210511 \
             --image-project ubuntu-os-cloud \
             --boot-disk-size {boot_disk_size}GB \
-            --boot-disk-type hyperdisk-standard \
+            --boot-disk-type hyperdisk-balanced \
             --scopes cloud-platform,compute-rw \
             --tags=wolfcontroller \
             --metadata=enable-oslogin=TRUE,wolfuser={wolfuser}"
