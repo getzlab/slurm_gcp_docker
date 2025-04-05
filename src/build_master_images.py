6adb608853788622ab340077a2710c6cefdb8c2e
SamWisedev <swiseman@broadinstitute.org>
2025-04-05 01:23:31 -0400

    use tunnel-through-iap

--- src/build_master_images.py
+++ src/build_master_images.py
@@ -125,7 +125,7 @@ if __name__ == "__main__":
 		# wait for instance to be ready
 		subprocess.check_call("""
 		  echo -n "Waiting for dummy instance to be ready ..."
-		  while ! gcloud compute --project {proj} ssh {host} --zone {zone} -- -o "UserKnownHostsFile /dev/null" \
+		  while ! gcloud compute --project {proj} ssh --tunnel-through-iap {host} --zone {zone} -- -o "UserKnownHostsFile /dev/null" \
 		    "[ -f /started ]" &> /dev/null; do
 			  sleep 1
 			  echo -n ".";
@@ -140,7 +140,7 @@ if __name__ == "__main__":
 
 		tmp = tempfile.mktemp()
 		subprocess.check_call("sudo docker save broadinstitute/slurm_gcp_docker_n4:latest broadinstitute/slurm_gcp_docker_n4:{} > {}".format(VERSION, tmp), shell=True)
-		subprocess.check_call('gcloud compute --project {proj} scp --tunnel-through-iap {src} {host}:/tmp/tmp_docker_file --zone {zone} && gcloud compute --project {proj} ssh {host} --zone {zone} -- -o "UserKnownHostsFile /dev/null" sudo touch /data_transferred'.format(proj = proj, src=tmp, host=host, zone=zone), shell=True)
+		subprocess.check_call('gcloud compute --project {proj} scp --tunnel-through-iap {src} {host}:/tmp/tmp_docker_file --zone {zone} && gcloud compute --project {proj} ssh --tunnel-through-iap {host} --zone {zone} -- -o "UserKnownHostsFile /dev/null" sudo touch /data_transferred'.format(proj = proj, src=tmp, host=host, zone=zone), shell=True)
 		os.remove(tmp)
 
 		#
