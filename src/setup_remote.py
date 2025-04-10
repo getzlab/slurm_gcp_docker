0128a4a21038fceb9239ca5a7a601f70a172a178
SamWisedev <swiseman@broadinstitute.org>
2025-04-10 14:40:47 -0400

    use custom docker image

--- src/setup_remote.py
+++ src/setup_remote.py
@@ -84,7 +84,7 @@ def create_wolfcontroller(instance_name, project=None, zone=None, machine_type="
 
     ## Copy stuff and run user script
     subprocess.check_call(
-        f"gcloud compute scp {SLURM_GCP_DOCKER_DIR}/ {instance_name}:slurm_gcp_docker --zone {zone} --recurse --project {project} --quiet --scp-flag='-q'", shell=True
+        f"gcloud compute scp {SLURM_GCP_DOCKER_DIR}/ {instance_name}:slurm_gcp_docker_balanced --zone {zone} --recurse --project {project} --quiet --scp-flag='-q'", shell=True
     )
     subprocess.check_call(
         f"gcloud compute scp ~/.config/gcloud/ {instance_name}:copied_gcloud_dir --zone {zone} --recurse --project {project} --quiet --scp-flag='-q'", shell=True
