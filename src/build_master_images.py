341b125581dc03fbd0456e531ab31db2909cd113
SamWisedev <swiseman@broadinstitute.org>
2025-04-10 15:42:32 -0400

    tunnel through iap

--- src/build_master_images.py
+++ src/build_master_images.py
@@ -90,19 +90,19 @@ if __name__ == "__main__":
 	if not args.skip_docker_image_build:
 		subprocess.check_call(f"""
 		  (cd .. &&
-		  sudo docker build --squash -t broadinstitute/slurm_gcp_docker:{VERSION} \
-			-t broadinstitute/slurm_gcp_docker:latest \
+		  sudo docker build --squash -t broadinstitute/slurm_gcp_docker_balanced_balanced:{VERSION} \
+			-t broadinstitute/slurm_gcp_docker_balanced_balanced:latest \
 			-f src/Dockerfile .)""", shell = True
 		)
 
 		if not args.skip_docker_image_push:
 			subprocess.check_call(f"""
-			  docker tag broadinstitute/slurm_gcp_docker:{VERSION} \
-				gcr.io/{proj}/slurm_gcp_docker:{VERSION} && \
-			  docker tag broadinstitute/slurm_gcp_docker:{VERSION} \
-				gcr.io/{proj}/slurm_gcp_docker:latest && \
-			  docker push gcr.io/{proj}/slurm_gcp_docker:{VERSION} && \
-			  docker push gcr.io/{proj}/slurm_gcp_docker:latest""",
+			  docker tag broadinstitute/slurm_gcp_docker_balanced:{VERSION} \
+				gcr.io/{proj}/slurm_gcp_docker_balanced:{VERSION} && \
+			  docker tag broadinstitute/slurm_gcp_docker_balanced:{VERSION} \
+				gcr.io/{proj}/slurm_gcp_docker_balanced:latest && \
+			  docker push gcr.io/{proj}/slurm_gcp_docker_balanced:{VERSION} && \
+			  docker push gcr.io/{proj}/slurm_gcp_docker_balanced:latest""",
 			  shell = True
 			)
 
@@ -127,7 +127,7 @@ if __name__ == "__main__":
 		# wait for instance to be ready
 		subprocess.check_call("""
 		  echo -n "Waiting for dummy instance to be ready ..."
-		  while ! gcloud compute --project {proj} ssh {host} --zone {zone} -- -o "UserKnownHostsFile /dev/null" \
+		  while ! gcloud compute --project {proj} ssh --tunnel-through-iap {host} --zone {zone} -- -o "UserKnownHostsFile /dev/null" \
 		    "[ -f /started ]" &> /dev/null; do
 			  sleep 1
 			  echo -n ".";
@@ -141,15 +141,15 @@ if __name__ == "__main__":
 		print("Transfering slurm docker image to dummy host ...")
 
 		tmp = tempfile.mktemp()
-		subprocess.check_call("sudo docker save broadinstitute/slurm_gcp_docker:latest broadinstitute/slurm_gcp_docker:{} > {}".format(VERSION, tmp), shell=True)
-		subprocess.check_call('gcloud compute --project {proj} scp {src} {host}:/tmp/tmp_docker_file --zone {zone} && gcloud compute --project {proj} ssh {host} --zone {zone} -- -o "UserKnownHostsFile /dev/null" sudo touch /data_transferred'.format(proj = proj, src=tmp, host=host, zone=zone), shell=True)
+		subprocess.check_call("sudo docker save broadinstitute/slurm_gcp_docker_balanced:latest broadinstitute/slurm_gcp_docker_balanced:{} > {}".format(VERSION, tmp), shell=True)
+		subprocess.check_call('gcloud compute --project {proj} scp --tunnel-through-iap {src} {host}:/tmp/tmp_docker_file --zone {zone} && gcloud compute --project {proj} ssh --tunnel-through-iap {host} --zone {zone} -- -o "UserKnownHostsFile /dev/null" sudo touch /data_transferred'.format(proj = proj, src=tmp, host=host, zone=zone), shell=True)
 		os.remove(tmp)
 
 		#
 		# wait for startup script to be completed
 		subprocess.check_call("""
 		  echo -n "Waiting for dummy instance to complete startup script ..."
-		  while ! gcloud compute --project {proj} ssh {host} --zone {zone} -- -o "UserKnownHostsFile /dev/null" \
+		  while ! gcloud compute --project {proj} ssh --tunnel-through-iap {host} --zone {zone} -- -o "UserKnownHostsFile /dev/null" \
 		    "[ -f /completed ]" &> /dev/null; do
 			  sleep 1
 			  echo -n ".";
