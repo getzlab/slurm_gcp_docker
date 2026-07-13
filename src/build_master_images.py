d9465213f48ae9e8cab7a5075c3888facb76e581
Sam Wiseman <swiseman@broadinstitute.org>
2026-07-13 13:15:20 -0400

    document, add test-mode parameter, update image family, fix local image

--- src/build_master_images.py
+++ src/build_master_images.py
@@ -23,7 +23,7 @@ Note that the Docker daemon must have experimental features enabled;
 add { "experimental": true } to /etc/docker/daemon.json
 """, formatter_class = argparse.RawTextHelpFormatter)
 	parser.add_argument('--image_prefix', '-i', help = "Prefix of image name", default = "wolf-worker-image")
-	parser.add_argument('--image_family', '-f', help = "Family to add image to", default = "slurm-gcp-docker-v2")
+	parser.add_argument('--image_family', '-f', help = "Family to add image to", default = "slurm-gcp-docker-v3")
 	parser.add_argument('--zone', '-z', help = "Compute zone to create dummy instance in", default = zone)
 	parser.add_argument('--project', '-p', help = "Compute project to create image in", default = "broad-getzlab-workflows")
 	parser.add_argument('--dummyhost', '-d', help = "Name of dummy VM image gets built on", default = "dummyhost")
@@ -31,6 +31,7 @@ add { "experimental": true } to /etc/docker/daemon.json
 	parser.add_argument('--skip_docker_image_push', help = "Whether to skip pushing Docker image to centeralized container regisitry", action = "store_true")
 	parser.add_argument('--skip_vm_image_build', help = "Skip building the worker VM image, i.e. only build the Docker image", action = "store_true")
 	parser.add_argument('--skip_docker_image_build', help = "Skip building the Docker image, i.e. only build the VM image", action = "store_true")
+	parser.add_argument('--test_mode', help = "Skip pushing 'latest' tag to image in centeralized container regisitry", action = "store_true")
 
 	args = parser.parse_args()
 
@@ -87,10 +88,14 @@ if __name__ == "__main__":
 	# 1. build Docker image
 	#
 
+	# gcr.io stores the remote docker image for the controller VM
+	# a local copy is built and tagged broadinstitute/slurm_gcp_docker:[version] for use by the worker VMs
+	# that local copy is stored on a VM image in the slurm-gcp-docker-[version] family
 	if not args.skip_docker_image_build:
 		subprocess.check_call(f"""
 		  (cd .. &&
-		  sudo docker build --squash -t broadinstitute/slurm_gcp_docker:{VERSION} \
+		  sudo docker build --squash -t broadinstitute/slurm_gcp_docker:v{VERSION} \
+			-t broadinstitute/slurm_gcp_docker:latest \
 			-f src/Dockerfile .)""", shell = True
 		)
 
@@ -98,7 +103,14 @@ if __name__ == "__main__":
 			subprocess.check_call(f"""
 			  docker tag broadinstitute/slurm_gcp_docker:v{VERSION} \
 				gcr.io/{proj}/slurm_gcp_docker:v{VERSION} && \
+			  docker tag broadinstitute/slurm_gcp_docker:{VERSION} \
+				gcr.io/{proj}/slurm_gcp_docker:latest && \
 			  docker push gcr.io/{proj}/slurm_gcp_docker:v{VERSION}""",
+			  shell = True
+			)
+		if not args.test_mode:
+			subprocess.check_call(f"""
+			  docker push gcr.io/{proj}/slurm_gcp_docker:latest""",
 			  shell = True
 			)
 
